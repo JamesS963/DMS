30202bacbcb117c3660a416a63bb91fe670c3a42
James Scott <JamesScott96@live.co.uk>
2017-02-26 04:32:01 +0000

    project added to the new repository

---
+++ src/main/java/main/DocumentDao.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Lewis on 23/02/2017.
+ */
+public interface DocumentDao extends CrudRepository<Document, Long> {
+
+
+
+
+}
